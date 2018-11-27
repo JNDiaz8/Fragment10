5dfa7742bb80b3b4df96511cc7e4f6ea488a849f
Joaquin Diaz Ramirez <joaquin.diaz.ramirez.8897@gmail.com>
2018-11-27 14:19:08 +0100

    Subida del Proyecto Fragment10. Tarea de acabarlo en casa.

---
+++ app/src/main/java/com/example/matinal/fragment10/Fragmento1.java
@@ -0,0 +1,14 @@
+package com.example.matinal.fragment10;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragmento1 extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/matinal/fragment10/fragment10.java
@@ -0,0 +1,13 @@
+package com.example.matinal.fragment10;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class fragment10 extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment10);
+    }
+}
